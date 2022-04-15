7ce4644f3512a9009c8fdc7b965df9cbb2969f43
10NN <dsloan2000@cox.net>
2022-04-15 15:18:36 -0700

    Added Leader classes

---
+++ Leader/src/main/java/Client.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.json.*;
+
+public class Client {
+    private int clientID = 1000000000;
+    
+    public Client() {
+        this.clientID = 1000000000;
+    } // end constructor
+} // end Client
---
+++ Leader/src/main/java/Node.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.json.*;
+
+public class Node {
+    private int money = 0;
+    private boolean leader = false;
+
+    public Node() {
+        this.leader = false;
+    } // end constructor
+
+    public static void main(String[] args) {
+        //
+    } // end main
+} // end Node
